0612aedf0c5022210489891a1d6e23705f0a7908
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-08-09 14:54:39 +0300

    deploy on web deck mob

--- src/components/view.rs
+++ src/components/view.rs
@@ -8,7 +8,6 @@ struct DogApi {
     message: String,
 }
 
-
 #[component]
 pub fn DogView() -> Element {
     // use_context_provider(|| TitleState("TitleInDegView".to_string()));
@@ -49,5 +48,82 @@ pub fn DogView() -> Element {
         }
         // Title2 {  }
 
+    }
+}
+
+
+
+
+
+fn DogGrid() -> Element {
+    rsx! {
+
+        SuspenseBoundary {
+
+            fallback: |_| rsx! {
+                div {
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    align_items: "center",
+                    justify_content: "center",
+                    "Loading..."
+                }
+            },
+
+            div {
+                display: "flex",
+                flex_direction: "column",
+                // Here you would typically map over a list of dog images
+                // For simplicity, we are just showing the DogView component
+                // You can replace this with actual dog images from your data source
+                DogViewGrid {}
+            }
+
+
+         }
+    }
+}
+
+#[derive(serde::Deserialize)]
+enum BreedResponse {
+    
+}  (String);
+
+
+
+#[component]
+pub fn DogViewGrid() -> Element {
+    let response = use_resource(move || async move {
+        // Artificially slow down the request to make the loading indicator easier to seer
+        // gloo_timers::future::TimeoutFuture::new(1000).await;
+        reqwest::Client::new()
+            .get(format!("https://dog.ceo/api/breed/{breed}/images"))
+            .send()
+            .await?
+            .json::<BreedResponse>()
+            .await
+    })
+    // Calling .suspend()? will suspend the component and return early while the future is running
+    .suspend()?;
+
+    // Then you can just handle the happy path with the resolved future
+    rsx! {
+        div {
+            display: "flex",
+            flex_direction: "row",
+            match &*response.read() {
+                Ok(urls) => rsx! {
+                    for image in urls.iter().take(3) {
+                        img {
+                            src: "{image}",
+                            width: "100px",
+                            height: "100px",
+                        }
+                    }
+                },
+                Err(err) => rsx! { "Failed to fetch response: {err}" },
+            }
+        }
     }
 }
